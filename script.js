7a99246e62031f1b8cd4a6cba2a71b406fbec300
Nicholas Tobat <ntobat@gmail.com>
2018-01-28 17:10:27 -0500

    redo!

--- script.js
+++ script.js
@@ -1,77 +1,20 @@
-//click
+//more about me
 $(function(){
-  $(".arrow-down").click(function(){
-    if( $(this).hasClass("downed") ){
-      $(this).removeClass("downed").addClass("upped");
-      $("html,body").animate({
-        scrollTop: $(".arrow-down").offset().top
-      }, 'slow');
-      $(".arrow-left").css('transform', 'translateX(-50vw)');
-      $(".arrow-right").css('transform', 'translateX(50vw)');
-    } else if ( $(this).hasClass("upped") ){
-      $(this).removeClass("upped").addClass("downed");
-      $("html,body").animate({
-        scrollTop: $(".header").offset().top
-      }, 'slow');
-      $(".arrow-left").css('transform', 'translateX(0)');
-      $(".arrow-right").css('transform', 'translateX(0)');
-    } else {
-      $(this).addClass("upped");
-      $("html,body").animate({
-        scrollTop: $(".arrow-down").offset().top
-      }, 'slow');
-      $(".arrow-left").css('transform', 'translateX(-50vw)');
-      $(".arrow-right").css('transform', 'translateX(50vw)');
-    };
+  $(".btn-left").click(function(){
+    $(".sec1").toggleClass("transform-out");
+    $(".sec2").toggleClass("transform-in");
+    $("#nav-1").toggleClass("nav-on");
   });
-
-	var pos = 1;
-  $(".arrow-right").click(function(){
-  	if(pos === 1){
-  		$(".sec1").animate({left: '-50%'});
-    	$(".sec2").animate({left: '50%'});
-    	pos += 1;
-  	} else if(pos === 2){
-  		$(".sec2").animate({left: '-50%'});
-  		$(".sec3").animate({left: '50%'});
-  		pos += 1;
-  	} else if(pos === 3){
-  		$(".sec3").animate({left: '150%'});
-  		$(".sec2").animate({left: '150%'});
-  		$(".sec1").animate({left: '50%'});
-  		pos = 1;
-  	} else if(pos === 0){
-	  	$(".sec0").animate({left: '-50%'});
-    	$(".sec1").animate({left: '50%'});
-    	pos += 1;
-    } else if(pos === -1){
-	  	$(".sec-1").animate({left: '-50%'});
-    	$(".sec0").animate({left: '50%'});
-    	pos += 1;
-    };
+  $("#nav-1").click(function(){
+    $(".sec1").toggleClass("transform-out");
+    $(".sec2").toggleClass("transform-in");
+    $("#nav-1").toggleClass("nav-on");
   });
-  $(".arrow-left").click(function(){
-  	if(pos === 1){
-  		$(".sec1").animate({left: '150%'});
-    	$(".sec0").animate({left: '50%'});
-    	pos -= 1;
-  	} else if(pos === 0){
-  		$(".sec0").animate({left: '150%'});
-    	$(".sec-1").animate({left: '50%'});
-    	pos -= 1;
-  	} else if(pos === -1){
-	    $(".sec-1").animate({left: '-50%'});
-	    $(".sec0").animate({left: '-50%'});
-	    $(".sec1").animate({left: '50%'});
-	    pos = 1;
-	  } else if(pos === 2){
-	  	$(".sec2").animate({left: '150%'});
-    	$(".sec1").animate({left: '50%'});
-    	pos -= 1;
-	  } else if(pos === 3){
-	  	$(".sec3").animate({left: '150%'});
-    	$(".sec2").animate({left: '50%'});
-    	pos -= 1;
+  $("#logo").click(function(){
+    if( $("#nav-1").hasClass("nav-on") ){
+      $(".sec1").toggleClass("transform-out");
+      $(".sec2").toggleClass("transform-in");
+      $("#nav-1").toggleClass("nav-on");
     };
   });
 });
