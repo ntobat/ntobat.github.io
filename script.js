882a0d1aae7ee86fb64763768c299165b2016011
Nicholas Tobat <ntobat@gmail.com>
2018-02-01 03:45:13 -0500

    pushin

--- script.js
+++ script.js
@@ -17,19 +17,18 @@ $(function(){
       $("#nav-1").toggleClass("nav-on");
     };
   });
-  //work nav
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
+  //smooth scroll
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 400, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    };
+  });
 });
 });
