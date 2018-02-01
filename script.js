325ef16dc97d5ca241d96f50e8933363cd7a5da8
Nicholas Tobat <ntobat@gmail.com>
2018-01-31 22:15:08 -0500

    brands

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//more about me
 $(function(){
+  //more about me
   $(".btn-left").click(function(){
     $(".sec1").toggleClass("transform-out");
     $(".sec2").toggleClass("transform-in");
@@ -17,4 +17,19 @@ $(function(){
       $("#nav-1").toggleClass("nav-on");
     };
   });
+  //work nav
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
 });
