05c687d2e4ea04e08fd40e649a0175ae817a5e9f
Nicholas Tobat <ntobat@gmail.com>
2018-02-15 01:57:19 -0500

    mmmm

--- scripts.js
+++ scripts.js
@@ -54,7 +54,12 @@ $(function(){
 	});
 
 	//back-nav from work item
-	//var referrer = document.referrer;
+	var referrer = document.referrer;
+	if( referrer === ( 'nicktobat.com/screens/topo.html')){
+		$(".container-3").hide();
+		$(".container-6").css("visibility", "visible").animate({ opacity: 1 });
+		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <a href=\"work.html\">work</a> <p>&nbsp;/&nbsp;</p> <a href=\"\" onclick=\"return false;\">screens</a> </div>" );
+	};
 
 });
 
