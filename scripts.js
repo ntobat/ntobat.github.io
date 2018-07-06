70ee9b0fe982ba967ff9c0be7c0344da57e724e8
Nicholas Tobat <ntobat@gmail.com>
2018-07-06 19:15:41 -0400

    m

--- scripts.js
+++ scripts.js
@@ -1,130 +1,5 @@
 $(function(){
 
-	//detect mobile
-	function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-	};
-	if( isMobileDevice() ){
-		var viewportHeight = $(".page").outerHeight();
-		$(".page").css({ height: window.innerHeight })
-	};
-	console.log(isMobileDevice());
-
-	//home page animate
-	var atypical = function(){
-		$(".container-1").hide();
-		$(".atypical").css("visibility", "visible").animate({ opacity: 1 }, 2000);
-		$(".atypical").fadeOut(2000);
-		setTimeout(function(){
-			$(".container-2").css("visibility", "visible").animate({ opacity: 1 }, 800);
-		}, 4000);
-	};
-	$(".btn-1").click(function(){
-		setTimeout(atypical, 800);
-		$(".container-1").fadeOut(800);
-	});
-
-	var hideit = function(){
-		$(".container-1").hide();
-		$(".container-2").css("visibility", "visible").animate({ opacity: 1 });
-	};
-	$(".btn-1").click(function(){
-		setTimeout(hideit, 800);
-		$(".container-1").fadeOut(800);
-	});
-
-	//work button animate - prevent immediate navigation
-	$(".btn-2").click(function(e){
-    e.preventDefault();
-    var goTo = this.getAttribute("href");
-    $(".container-2").fadeOut(800);
-    setTimeout(function(){
-      window.location = goTo;
-    }, 800);       
-	});
-
-	//work page animate-brand
-	var hidebrand = function(){
-		$(".container-3").hide();
-		$(".container-4").css({ "visibility": "visible", "display": "block" }).animate({ opacity: 1 });
-		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <a id=\"workbread\" href=\"#work\">work</a> <p>&nbsp;/&nbsp;</p> <p>branding</p> </div>" );
-	}
-	$(".brand").click(function(e){
-		e.preventDefault();
-		setTimeout(hidebrand, 800);
-		$(".container-3").fadeOut(800).animate({ opacity: 0 }, 800);
-	});
-
-	/*//work page animate-logo
-	var hidelogo = function(){
-		$(".container-3").hide();
-		$(".container-5").css({ "visibility": "visible", "display": "block" }).animate({ opacity: 1 });
-		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <a id=\"workbread\" href=\"work.html\">work</a> <p>&nbsp;/&nbsp;</p> <p>logos</p> </div>" );
-	}
-	$(".logo").click(function(){
-		setTimeout(hidelogo, 800);
-		$(".container-3").fadeOut(800).animate({ opacity: 0 }, 800);
-	});
-	*/
-	//work page animate-exp
-	var hideexp = function(){
-		$(".container-3").hide();
-		$(".container-5").css({ "visibility": "visible", "display": "block" }).animate({ opacity: 1 });
-		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <a id=\"workbread\" href=\"work.html\">work</a> <p>&nbsp;/&nbsp;</p> <p>experience</p> </div>" );
-	}
-	$(".logo").click(function(e){
-		e.preventDefault();
-		setTimeout(hideexp, 800);
-		$(".container-3").fadeOut(800).animate({ opacity: 0 }, 800);
-	});
-
-	//work page animate-scrn
-	var hidescrn = function(){
-		$(".container-3").hide();
-		$(".container-6").css({ "visibility": "visible", "display": "block" }).animate({ opacity: 1 });
-		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <a id=\"workbread\" href=\"work.html\">work</a> <p>&nbsp;/&nbsp;</p> <p>screens</p> </div>" );
-	}
-	$(".scrn").click(function(e){
-		e.preventDefault();
-		setTimeout(hidescrn, 800);
-		$(".container-3").fadeOut(800).animate({ opacity: 0 }, 800);
-	});
-
-	//work page animate-fun
-	var hidefun = function(){
-		$(".container-3").hide();
-		$(".container-7").css({ "visibility": "visible", "display": "block" }).animate({ opacity: 1 });
-		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <a id=\"workbread\" href=\"work.html\">work</a> <p>&nbsp;/&nbsp;</p> <p>fun</p> </div>" );
-	}
-	$(".fun").click(function(e){
-		e.preventDefault();
-		setTimeout(hidefun, 800);
-		$(".container-3").fadeOut(800).animate({ opacity: 0 }, 800);
-	});
-
-	//work button animate - prevent immediate navigation
-	var backtowork = function(){
-		$("div.workcat").hide();
-		$(".container-3").css({ "visibility": "visible", "display": "block" }).animate({ opacity: 1 });
-		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <p>work</p> </div>" );
-	}
-	$("body").on("click", "#workbread", function(e){
-    e.preventDefault();
-    setTimeout(backtowork, 800);
-    $("div.workcat").fadeOut(800).animate({ opacity: 0 }, 800);
-	});
-	//work page animate-back-to-work
-/*	var resetwork = function(){
-		$(".workcat").hide();
-		$(".container-3").css({ "visibility": "visible", "display": "block" }).animate({ opacity: 1 });
-		$(".bread").replaceWith( "<div class=\"bread\"> <a href=\"index.html\">nicktobat</a> <p>&nbsp;/&nbsp;</p> <p>work</p> </div>" );
-	}
-	$("#workbread").click(function(){
-		//setTimeout(resetwork, 800);
-		//$(".workcat").fadeOut(800);
-		$(".workcat").css({ "visibility": "hidden", "display": "none" }).animate({ opacity: 0 });
-	});  */
-
 	//background color animation
 	$(".skin").click(function(e){
 		e.preventDefault();
@@ -273,7 +148,7 @@ $(function(){
     }, 1800);
 	});
 
-	//scroll fade header nav
+	//entry: scroll fade header nav
 	$(window).scroll(function(){
 		var height = $(window).scrollTop();
 
