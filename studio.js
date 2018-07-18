dac3de8f9b9aebf2be26fac126c89eacc228b9de
Nicholas Tobat <ntobat@gmail.com>
2018-07-17 20:02:47 -0400

    ixd grid

--- studio.js
+++ studio.js
@@ -12,12 +12,14 @@ $(function(){
 			$("#ixdLeftImg").animate({ opacity: 0.15 }, 25);
 			$("#ixdRgtImg").animate({ opacity: 0 }, 25);
 			$(".ixdgif").attr("src", "assets/studio/menu.gif");
+			$(".activeDot").removeClass('activeDot').next().addClass('activeDot');
 			ixdPos += 1;
 		} else if ( ixdPos === 1 ){
 			$(".ixdCaro").css("transform", "translateX(0)");
 			$("#ixdRgtImg").animate({ opacity: 0.75 }, 25);
 			$("#ixdLeftImg").animate({ opacity: 0.75 }, 25);
 			$(".ixdgif").attr("src", "assets/studio/product.gif")
+			$(".activeDot").removeClass('activeDot').next().addClass('activeDot');
 			ixdPos += 1;
 		}
 		
@@ -33,12 +35,14 @@ $(function(){
 			$("#ixdRgtImg").animate({ opacity: 0.15 }, 25);
 			$("#ixdLeftImg").animate({ opacity: 0 }, 25);
 			$(".ixdgif").attr("src", "assets/studio/reserve.gif")
+			$(".activeDot").removeClass('activeDot').prev().addClass('activeDot');
 			ixdPos -= 1;
 		} else if ( ixdPos === 3 ){
 			$(".ixdCaro").css("transform", "translateX(0)");
 			$("#ixdLeftImg").animate({ opacity: 0.75 }, 25);
 			$("#ixdRgtImg").animate({ opacity: 0.75 }, 25);
 			$(".ixdgif").attr("src", "assets/studio/product.gif");
+			$(".activeDot").removeClass('activeDot').prev().addClass('activeDot');
 			ixdPos -= 1;
 		}
 	});
@@ -47,11 +51,13 @@ $(function(){
 	$(".menu").click(function(e){
 		e.preventDefault();
 		if( !$(".nav").hasClass("navOut") ){
+			$(".headerBlack").css("background-color", "white");
 			$(".nav").addClass("navOut");
 			$("body").addClass("navModal");
 			$("#menuWhite").replaceWith( "<img id=\"navClose\" src=\"assets/menuCloseBlack.svg\" />");
 			$("#logo").replaceWith( "<img id=\"logo\" src=\"assets/logoBlack.png\" />");
 		} else {
+			$(".headerBlack").css("background-color", "black");
 			$(".nav").removeClass("navOut");
 			$("body").removeClass("navModal");
 			$("#navClose").replaceWith( "<img id=\"menuWhite\" src=\"assets/menu.svg\" />");
